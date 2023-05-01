b57f0e9a1ee62db0bb21a6e52f055086018b8425
Ivan Ivanov <dudleyivanov@gmai.com>
2023-05-01 20:31:45 +0300

    remove log

--- src/Components/File.js
+++ src/Components/File.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useFs } from '../context'
 
 const ContextMenu = ({api, Key}) => {
-    console.log(api)
     const deleteFile = () => {
         api({type: 'delete', Key})
     }
